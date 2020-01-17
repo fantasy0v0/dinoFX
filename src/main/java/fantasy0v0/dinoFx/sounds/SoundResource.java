d179240796831a4560fda9aa29eaa6eda93f8f23
Fan <xiaofanms@outlook.com>
2020-01-17 09:47:28 +0800

    更名。

--- src/main/java/fantasy0v0/dinoFx/sounds/SoundResource.java
+++ src/main/java/fantasy0v0/dinoFx/sounds/SoundResource.java
@@ -3,31 +3,31 @@ package fantasy0v0.dinoFx.sounds;
 import javafx.scene.media.AudioClip;
 
 /**
- * 音效
- *
+ * 音效资源
+ * <p>
  * Fan 1/16/2020
  */
-public final class SoundDefinition {
+public final class SoundResource {
 
   /**
    * 按钮按下
    */
   public final static AudioClip BUTTON_PRESS = new AudioClip(
-    SoundDefinition.class.getResource("/sounds/BUTTON_PRESS.mp3").toExternalForm());
+    SoundResource.class.getResource("/sounds/BUTTON_PRESS.mp3").toExternalForm());
 
   /**
    * 碰撞
    */
   public final static AudioClip HIT = new AudioClip(
-    SoundDefinition.class.getResource("/sounds/HIT.mp3").toExternalForm());
+    SoundResource.class.getResource("/sounds/HIT.mp3").toExternalForm());
 
   /**
    * 碰撞
    */
   public final static AudioClip SCORE = new AudioClip(
-    SoundDefinition.class.getResource("/sounds/SCORE.mp3").toExternalForm());
+    SoundResource.class.getResource("/sounds/SCORE.mp3").toExternalForm());
 
-  private SoundDefinition() {
+  private SoundResource() {
 
   }
 }
